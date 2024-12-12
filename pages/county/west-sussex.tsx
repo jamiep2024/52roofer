180fbeb05068b1679bb3b93db14a6e0e4aa14389
jamiep2024 <Jpoynton25@gmail.com>
2024-12-12 20:08:53 +0000

    Fix type error in West Sussex county page

--- pages/county/west-sussex.tsx
+++ pages/county/west-sussex.tsx
@@ -1,15 +1,13 @@
 import { GetStaticProps } from 'next';
 import { serviceAreas } from '../../data/serviceAreas';
-import { businesses } from '../../data/businesses';
 import CountyLandingPage from '../../components/CountyLandingPage';
 
-export default function WestSussex({ businesses }: { businesses: any[] }) {
+export default function WestSussex() {
   const county = serviceAreas.westSussex;
   
   return (
     <CountyLandingPage
       county={county.name}
-      businesses={businesses}
       mainTowns={county.mainTowns}
       postcodes={county.postcodes}
     />
@@ -17,14 +15,8 @@ export default function WestSussex({ businesses }: { businesses: any[] }) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const countyBusinesses = businesses.filter(
-    (business) => business.location === serviceAreas.westSussex.name
-  );
-
   return {
-    props: {
-      businesses: countyBusinesses,
-    },
+    props: {},
     revalidate: 3600, // Revalidate every hour
   };
 };
