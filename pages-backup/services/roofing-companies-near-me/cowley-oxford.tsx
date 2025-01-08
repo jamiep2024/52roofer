52afc36c2c645a6cdeedfd79405ab73b496615e3
jamiep2024 <Jpoynton25@gmail.com>
2025-01-08 17:19:32 +0000

    Updated dynamic routing for roofers-in location

--- pages-backup/services/roofing-companies-near-me/cowley-oxford.tsx
+++
@@ -1,8 +0,0 @@
-export default function RoofingCompaniesCowleyOxford() {
-  return (
-    <div>
-      <h1>Roofing Companies in Cowley, Oxford</h1>
-      <p>Content for roofing companies in Cowley, Oxford.</p>
-    </div>
-  );
-}
--- pages-backup/services/roofing-companies-near-me/salisbury-wiltshire.tsx
+++
@@ -1,8 +0,0 @@
-export default function RoofingCompaniesSalisburyWiltshire() {
-  return (
-    <div>
-      <h1>Roofing Companies in Salisbury, Wiltshire</h1>
-      <p>Content for roofing companies in Salisbury, Wiltshire.</p>
-    </div>
-  );
-}
