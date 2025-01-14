25c84d82ca769d3651b8c1cafeab03a929ff9825
jamiep2024 <Jpoynton25@gmail.com>
2025-01-14 13:47:26 +0000

    Removed ServiceAreas import and updated ESLint config

--- components/templates/LocationPageTemplate.tsx
+++ components/templates/LocationPageTemplate.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react';
 import Layout from '../layout/Layout';
 import Hero from '../Hero';
-import ServiceAreas from '../ServiceAreas';
 
 interface LocationPageTemplateProps {
   locationName: string;
@@ -20,7 +19,7 @@ export default function LocationPageTemplate({
         title={`Roofing Services in ${locationName}`}
         image={heroImage}
       />
-      <ServiceAreas location={locationName} />
+      
       {children}
     </Layout>
   );
